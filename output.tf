a0b4e610cf3588582ed7d44ae060482a1a8cb49b
alexander.gramovich <alexander.gramovich@inspectorio.com>
2019-06-17 14:02:56 +0300

    Add sftp server output

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "sftp_server_arn" {
+  value = aws_transfer_server.sftp_server.arn
+}
+
+output "sftp_server_id" {
+  value = aws_transfer_server.sftp_server.id
+}
+
+output "sftp_server_endpoint" {
+  value = aws_transfer_server.sftp_server.endpoint
+}
